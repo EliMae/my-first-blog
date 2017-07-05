5169d660e451cc9e838ea294c51343cf2cad7665
EliMae3 <wildpuma3@gmail.com>
2017-07-05 16:38:07 -0400

    My git

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@-hqy6@h^o)reaih4x=@!!h(_=95d672i^8#)5j+$@9appsocw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'elimae.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
